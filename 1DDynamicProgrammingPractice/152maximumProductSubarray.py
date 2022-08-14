d6b7883dc3d92b7808c00bb8fc56733af780f003
kingofdreams777 <rossmartin@sandiego.edu>
2022-08-14 13:52:40 -0700

    stuff

---
+++ 1DDynamicProgrammingPractice/152maximumProductSubarray.py
@@ -0,0 +1,36 @@
+"""
+Simple brute force solution.
+Works when all integers are positive
+"""
+"""
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        product = 0
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                subarray = nums[i:j]
+                potential = 1
+                for k in subarray:
+                    potential *= k
+                product = max(product, potential)
+
+        
+        return product
+"""
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        res = max(nums) # 0 -> [-1] need to set it to something
+        curMin, curMax = 1, 1
+
+        for n in nums:
+            if n == 0:
+                curMin, curMax = 1, 1
+                continue
+            curMax = max(n * curMax, n * curMin, n)
+
+
+ex1 = [2,3,-2,4]
+ex2 = [-2,0,-1]
+s = Solution()
+print(s.maxProduct(ex1))
+print(s.maxProduct(ex2))
