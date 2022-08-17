384a7acc3668508b1f8652993fe8e84d7709ec1e
kingofdreams777 <rossmartin@sandiego.edu>
2022-08-16 20:18:16 -0700

    updates

--- 1DDynamicProgrammingPractice/152maximumProductSubarray.py
+++ 1DDynamicProgrammingPractice/152maximumProductSubarray.py
@@ -19,14 +19,20 @@ class Solution:
 """
 class Solution:
     def maxProduct(self, nums: list[int]) -> int:
-        res = max(nums) # 0 -> [-1] need to set it to something
+        res = max(nums) # 0 -> [-1]
         curMin, curMax = 1, 1
 
         for n in nums:
             if n == 0:
-                curMin, curMax = 1, 1
+                curMin, curMax = 1,1
                 continue
+            tmp = curMax * n
             curMax = max(n * curMax, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
+            res = max(res, curMax, curMin)
+
+        return res
+
 
 
 ex1 = [2,3,-2,4]
